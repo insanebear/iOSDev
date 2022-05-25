2299c96868dc3f784bd032ce3803c3f3429df8ec
Jayde Jeong <yljung@se.kaist.ac.kr>
2022-05-25 19:01:48 +0900

    Initial commit for CollectionViewProject

---
+++ UIKit/CollectionViewProject/CollectionViewProject/CollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewProject
+//
+//  Created by Jayde Jeong on 2022/05/25.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
