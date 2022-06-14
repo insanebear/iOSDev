e23e778733b4fae6af16877faef900e46a914127
Jayde Jeong <yljung@se.kaist.ac.kr>
2022-06-14 14:42:58 +0900

    Added: [MapProject] TableView for NMapSearchResultViewController

--- UIKit/MapProject/MapProject/ViewController/NMapMaps/NMapSearchResultViewController.swift
+++ UIKit/MapProject/MapProject/ViewController/NMapMaps/NMapSearchResultViewController.swift
@@ -8,15 +8,76 @@
 import UIKit
 
 class NMapSearchResultViewController: UIViewController {
-
+    var searchBar: UISearchBar!
+    var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "검색"
         self.view.backgroundColor = UIColor(named: "backgroundColor")
-        // Do any additional setup after loading the view.
+        
+        setupSearchBar()
+        setupTableView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        searchBar.becomeFirstResponder()
+    }
+    
+    func setupSearchBar() {
+        searchBar = UISearchBar()
+        
+        searchBar.delegate = self
+        searchBar.placeholder = "검색어를 입력하세요"
+        searchBar.searchBarStyle = .minimal
+        searchBar.isTranslucent = true
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 5),
+            searchBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5)
+        ])
+    }
+    
+    func setupTableView() {
+        tableView = UITableView(frame: .zero, style: .insetGrouped)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 5),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
 
+extension NMapSearchResultViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+extension NMapSearchResultViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
 }
