5ec319049e5848ff2926b6ad1c4241692caedeb6
Jayde Jeong <yljung@se.kaist.ac.kr>
2022-06-20 19:45:47 +0900

    Added: [Weather] Initial commit

---
+++ SwiftUI/Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jayde Jeong on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
