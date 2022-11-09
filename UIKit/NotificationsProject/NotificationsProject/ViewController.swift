5c62a8c7b32393b84531d0f37688429c6b284620
Jayde Jeong <yljung@se.kaist.ac.kr>
2022-11-09 15:37:25 +0900

    Added: [NotificationsProject] Initial commit

---
+++ UIKit/NotificationsProject/NotificationsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotificationsProject
+//
+//  Created by Jayde Jeong on 2022/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
