506eda6aba119c93eb30acd91ce45a02f39577b4
Jayde Jeong <yljung@se.kaist.ac.kr>
2022-11-11 17:42:49 +0900

    Added: [CameraPhotosProject] Add camera

--- UIKit/CameraPhotosProject/CameraPhotosProject/ViewController.swift
+++ UIKit/CameraPhotosProject/CameraPhotosProject/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationBarDelegate {
 
+    var stackView: UIStackView!
+    var imageView: UIImageView!
+    var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setViews()
+        setConstraints()
     }
 
+    func setViews() {
+        imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(systemName: "photo")
+        imageView.layer.borderColor = UIColor.systemBlue.cgColor
+        imageView.layer.borderWidth = 1
 
+        imageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+
+        button = UIButton()
+        button.setTitle("Add a photo", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(didTappedButton(_:)), for: .touchUpInside)
+        
+        stackView = UIStackView(arrangedSubviews: [
+            imageView, button
+        ])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 20
+        
+        self.view.addSubview(stackView)
+    }
+    
+    func setConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+
+        ])
+    }
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @objc func didTappedButton(_ sender: UITapGestureRecognizer) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    
+    // open camera
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        // set the image took from the camera
+        imageView.image = image
+    }
 }
 
